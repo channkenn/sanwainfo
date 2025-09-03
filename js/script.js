0855203142f583d978695fcd48902bb837103146
channkenn <91776275+channkenn@users.noreply.github.com>
2025-09-03 15:50:08 +0900

    Create script.js
    サンワインフォLP

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("contactBtn");
+  btn.addEventListener("click", () => {
+    alert("お問い合わせフォームは現在準備中です！");
+  });
+});
